790bc6a06ef4049688def126684a38c496a4e6fd
Agustin Murad <juanagusmurad@gmail.com>
2025-10-29 11:44:54 -0300

    fear useRef

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
+
+//const gifsCache: Record<string, Gif[]> = {};
+
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousSearches, setPreviousSearches] = useState<string[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleSearchClicked = async (search: string) => {
+    if (gifsCache.current[search]) {
+      setGifs(gifsCache.current[search]);
+      return;
+    }
+
     const gifs = await getGifsByQuery(search);
     setGifs(gifs);
   };
+
   const handleSearch = async (query: string) => {
     query = query.trim().toLowerCase();
     if (query.length === 0) return;
@@ -24,6 +35,8 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
 
     setGifs(gifs);
+
+    gifsCache.current[query] = gifs;
   };
 
   return {
